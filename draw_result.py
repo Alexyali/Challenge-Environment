7e879d9aed80755fedb548d674554772100c8935
hwj <wjhuang@sjtu.edu.cn>
2021-12-09 00:05:36 +0800

    use function to get data from logs

--- draw_result.py
+++ draw_result.py
@@ -8,29 +8,27 @@ cldcc_delay_log = "paper_result/cldcc_delay.log"
 gcc_tput_log = "paper_result/gcc_tput.log"
 cldcc_tput_log = "paper_result/cldcc_tput.log"
 
+def get_delay_list(log_file:str):
+    delay = []
+    recv_rate = []
+    with open(log_file, "r") as f:
+        f.readline()
+        lines = f.readlines()
+        for row in lines:
+            data = row.strip().split()
+            delay.append(int(data[-1]))
+            recv_rate.append(float(data[1]))
+    return delay, recv_rate
+
 gcc_delay_list = []
-cldcc_delay_list = []
 gcc_recv_timestamp = []
+gcc_delay_list, gcc_recv_timestamp = get_delay_list(gcc_delay_log)
+
+cldcc_delay_list = []
 cldcc_recv_timestamp = []
-
-with open(gcc_delay_log, "r") as f:
-    f.readline()
-    lines = f.readlines()
-    for row in lines:
-        data = row.strip().split()
-        gcc_delay_list.append(int(data[-1]))
-        gcc_recv_timestamp.append(float(data[1]))
-
-with open(cldcc_delay_log, "r") as f:
-    f.readline()
-    lines = f.readlines()
-    for row in lines:
-        data = row.strip().split()
-        cldcc_delay_list.append(int(data[-1]))
-        cldcc_recv_timestamp.append(float(data[1]))
+cldcc_delay_list, cldcc_recv_timestamp = get_delay_list(cldcc_delay_log)
 
 plt.figure(figsize=(12,5))
-
 plt.plot(gcc_recv_timestamp, gcc_delay_list,color="red",label="Trendline-GCC")
 plt.plot(cldcc_recv_timestamp, cldcc_delay_list,color="blue", label="RTC-CLDCC")
 plt.legend(fontsize=15)
@@ -42,24 +40,23 @@ plt.savefig("paper_result/test_delay.png", dpi=300)
 plt.close()
 
 # draw throughput
-gcc_tput_list = []
-cldcc_tput_list = []
-with open(gcc_tput_log, "r") as f:
-    f.readline()
-    lines = f.readlines()
-    for row in lines:
-        data = row.strip()
-        gcc_tput_list.append(float(data))
+def get_tput_list(log_file:str):
+    tput_list = []
+    with open(log_file, "r") as f:
+        f.readline()
+        lines = f.readlines()
+        for row in lines:
+            data = row.strip()
+            tput_list.append(float(data))
+    return tput_list
 
-with open(cldcc_tput_log, "r") as f:
-    f.readline()
-    lines = f.readlines()
-    for row in lines:
-        data = row.strip()
-        cldcc_tput_list.append(float(data))
+gcc_tput_list = []
+gcc_tput_list = get_tput_list(gcc_tput_log)
+
+cldcc_tput_list = []
+cldcc_tput_list = get_tput_list(cldcc_tput_log)
 
 plt.figure(figsize=(12,5))
-
 plt.plot(gcc_tput_list,color="red",label="Trendline-GCC")
 plt.plot(cldcc_tput_list,color="blue", label="RTC-CLDCC")
 plt.legend(fontsize=15)
